8ab09a156f21c64a07efa9820c056807f531b328
Nicholas Schrimpf <nickschrimpf@users.noreply.github.com>
2022-12-13 14:45:55 -0700

    event propigation

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,7 +1,9 @@
-// const theButton = document.getElementById('theButton')
+const theButton = document.getElementById('theButton')
 // const theSecondButton = document.getElementById('theSecondButton')
 const circle = document.getElementById('circle');
-const allButtons = document.querySelectorAll('button');
+// const allButtons = document.querySelectorAll('button');
+
+const theDiv = document.querySelector('div')
 
 const theButtonClickHandler = (e)=>{
     // https://developer.mozilla.org/en-US/docs/Web/API/Event
@@ -9,6 +11,7 @@ const theButtonClickHandler = (e)=>{
     // https://developer.mozilla.org/en-US/docs/Web/API/Event/target
     console.log(e.target)
     // https://developer.mozilla.org/en-US/docs/Web/API/Event/bubbles
+    // DOES THIS EVENT PROPIGATE DOES IT BUBBLE UP TO ANCSESTORS
     console.log(e.bubbles)
    
     // e.target.disabled = true;
@@ -20,9 +23,9 @@ const theButtonClickHandler = (e)=>{
 // theButton.addEventListener('click',theButtonClickHandler)
 // theSecondButton.addEventListener('click',theButtonClickHandler)
 
-allButtons.forEach(btn => {
-    btn.addEventListener('click',theButtonClickHandler)
-});
+// allButtons.forEach(btn => {
+//     btn.addEventListener('click',theButtonClickHandler)
+// });
 
 // https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener
 // The removeEventListener() method of the EventTarget interface removes an event listener 
@@ -31,7 +34,7 @@ allButtons.forEach(btn => {
 // itself, and various optional options that may affect the matching process; see Matching event 
 // listeners for removal.
 
-// useCapture *Optional - interesting not sure what a capture listener is yet
+// useCapture *Optional 
 // A boolean value that specifies whether the event listener to be removed is registered as 
 // a capturing listener or not. If this parameter is absent, the default value false is assumed.
 
@@ -109,12 +112,63 @@ window.addEventListener('scroll', scrollHandler);
 
 const thisForm = document.querySelector('form')
 thisForm.addEventListener('submit',(e)=>{
+    // Event.preventDefault()
+    // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+    // The preventDefault() method of the Event interface tells the user agent that if
+    // the event does not get explicitly handled, its default action should not be taken as it normally would be.
     e.preventDefault()
     console.log(e)
 })
 console.log(thisForm)
-// Event.preventDefault()
-// https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-// The preventDefault() method of the Event interface tells the user agent that if
-// the event does not get explicitly handled, its default action should not be taken as it normally would be.
 
+// PROPIGATION - NOT JUST THE ELEMENT ITSELF BUT ON ANY ANCESTOR
+// theDiv.addEventListener('click', event => {
+//     console.log('theDiv')
+//     console.log(event)
+// })
+// theButton.addEventListener('click', event => {
+//     console.log('theButton')
+//     console.log(event)
+// })
+// STOP PROPIGATON STOP THE CLICK EVENT ON THE BUTTON
+// FROM TRIGGERING ON THE DIV
+theDiv.addEventListener('click', event => {
+    console.log('theDiv')
+    console.log(event)
+})
+theButton.addEventListener('click', event => {
+    event.stopPropagation()
+    console.log('theButton')
+    console.log(event)
+})
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// *** NOT USING PROPIGATION *** 
+// listItems.forEach(item => {
+//     item.addEventListener('click',(event)=> {
+//         event.target.classList.toggle('highlight')
+//     });
+// });
+
+// *** USING PROPIGATION TO ATTACH ONLY ONE EVENT LISTENER TO THE ANCSETOR OF ALL ITEMS
+
+list.addEventListener('click',event => {
+    // event.target.classList.toggle('highlight')// THIS WILL CAUSE PROBLEMS WITH MORE COMPLEX LIST ITEMS OR DIVS SEE RESULT HERE
+    console.log(event.currentTarget) //REFERS TO THE WHERE THE LISTENER WAS PLACED
+    event.target.closest('li').classList.toggle('highlight')
+})
+
+// theDiv.addEventListener('click', event => {
+//     console.log('theDiv')
+//     console.log(event)
+// },true)
+// useCapture *Optional 
+// A boolean value that specifies whether the event listener to be removed is registered as 
+// a capturing listener or not. If this parameter is absent, the default value false is assumed.
+// THIS WILL REVERSE THE ORDER OF THE PREVIOUS CONSOLE LOGS SO THE DIV WILL LOG FIRST
+// INSTEAD OF BUBBLING THIS WILL RUN IN THE CAPTURING PHASE INSTEAD OF BUBBLING
+// theButton.addEventListener('click', event => {
+//     console.log('theButton')
+//     console.log(event)
+// })
